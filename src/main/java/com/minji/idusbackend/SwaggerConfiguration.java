8369441065380ddce6d020ce566e3820fc92ddec
minz <ghdalswl77@naver.com>
2022-06-29 23:35:07 +0900

    swagger test

--- src/main/java/com/minji/idusbackend/SwaggerConfiguration.java
+++ src/main/java/com/minji/idusbackend/SwaggerConfiguration.java
@@ -1,34 +1,34 @@
-package com.minji.idusbackend;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
-
-@Configuration
-@EnableSwagger2
-@EnableAutoConfiguration
-public class SwaggerConfiguration {
-    private String version;
-
-    @Bean
-    public Docket apiV1() {
-        version = "V1";
-//        title = "littleVanilla API " + version;
-
-        return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .groupName(version)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.minji.idusbackend"))
-                .paths(PathSelectors.any())
-                .build();
-
-    }
-}
+//package com.minji.idusbackend;
+//
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//
+//
+//@Configuration
+//@EnableSwagger2
+//@EnableAutoConfiguration
+//public class SwaggerConfiguration {
+//    private String version;
+//
+//    @Bean
+//    public Docket apiV1() {
+//        version = "V1";
+////        title = "littleVanilla API " + version;
+//
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .useDefaultResponseMessages(false)
+//                .groupName(version)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.minji.idusbackend"))
+//                .paths(PathSelectors.any())
+//                .build();
+//
+//    }
+//}
