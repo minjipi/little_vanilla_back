b17dda5fb8e64acc2249e222d0c0038a3a2448f7
minz <ghdalswl77@naver.com>
2022-06-30 02:36:05 +0900

    swagger v1 작성

--- src/main/java/com/minji/idusbackend/SwaggerConfiguration.java
+++ src/main/java/com/minji/idusbackend/SwaggerConfiguration.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 
 
 @Configuration
@@ -16,11 +19,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAutoConfiguration
 public class SwaggerConfiguration {
     private String version;
+    private String title;
 
     @Bean
-    public Docket apiV1() {
+    public Docket api() {
         version = "V1";
-//        title = "littleVanilla API " + version;
+        title = "littleVanilla API " + version;
 
         return new Docket(DocumentationType.SWAGGER_2)
                 .useDefaultResponseMessages(false)
@@ -29,6 +33,19 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.basePackage("com.minji.idusbackend"))
                 .paths(PathSelectors.any())
                 .build();
+    }
 
+    private ApiInfo apiInfo(String title, String version) {
+        return new ApiInfo(
+                title,
+                " A Little Vanilla API Docs",
+                version,
+                "http://www.alittlevanilla.kro.kr/",
+                new Contact("Contact Me", "https://blog.naver.com/ghdalswl77", "ghdalswl77@naver.com"),
+                "Licenses",
+
+                "http://www.alittlevanilla.kro.kr/",
+
+                new ArrayList<>());
     }
 }
